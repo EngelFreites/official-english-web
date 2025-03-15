e8bfd07ad3adb8f1598fbd94259f28ad61be8169
Engelbert <engelfreites97@gmail.com>
2025-03-15 11:29:45 -0300

    feat(header): create basic structure with navigation menu

---
+++ src/component/Header/Header.jsx
@@ -0,0 +1,23 @@
+export default function Header() {
+  return (
+    <header className="header">
+      <h1>
+        <a href="/">Logo</a>
+      </h1>
+
+      <nav className="menu">
+        <ul>
+          <li>
+            <a href="/">Inicio</a>
+          </li>
+          <li>
+            <a href="/">Sobre nosotros</a>
+          </li>
+          <li>
+            <a href="/">Contacto</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
