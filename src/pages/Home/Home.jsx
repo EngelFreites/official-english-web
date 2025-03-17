a1a2006d6ea10e52a0d38ce5bc1868303e8c7dfc
Engelbert <engelfreites97@gmail.com>
2025-03-16 22:29:16 -0300

    feat(home): create home page

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,21 @@
+export default function Home() {
+  return (
+    <div className="layout">
+      <main className="home">
+        <h1>
+          <span className="highlight">
+            🚀 Domina el inglés y abre un mundo de oportunidades 🌍
+          </span>
+        </h1>
+        <h2>
+          Aprende de manera rápida, divertida y efectiva. ¡Comienza hoy y
+          transforma tu futuro!
+        </h2>
+        <button className="start-button">Start Now</button>
+      </main>
+      <secction className="content-img">
+        <div className="img"></div>
+      </secction>
+    </div>
+  );
+}
