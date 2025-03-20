ebfd88f1c3f2357732cd5cd01ba80be26166b33e
Engelbert <engelfreites97@gmail.com>
2025-03-20 00:01:19 -0300

    fix(plans): create new squema to plans

--- src/component/Plans/Plans.jsx
+++ src/component/Plans/Plans.jsx
@@ -1,62 +1,104 @@
 import "./plans.css";
-import { FaHandshake } from "react-icons/fa";
-import { IoMdSpeedometer } from "react-icons/io";
-import { FaAward } from "react-icons/fa";
-import { FaComments } from "react-icons/fa";
+import plans from "../../data/plans.js";
+import { useState } from "react";
+import { FcNext } from "react-icons/fc";
+import { FcFactory } from "react-icons/fc";
+import { FcGlobe } from "react-icons/fc";
+import { FcCollaboration } from "react-icons/fc";
+import { FcCalendar } from "react-icons/fc";
 
 export default function Plans() {
+  const [plan, setPlan] = useState("corporate");
   return (
     <div className="container-plans">
       <h1 className="title-plans">Nuestros Planes</h1>
-      <section className="container-card">
-        <div className="card-plan">
-          <div className="content-icon-plan-corporate">
-            <FaHandshake className="icon-plan" />
-          </div>
-          <h2>Plan Corporativo</h2>
-          <p className="text-plan">
-            Ofrecemos sesiones de inglés flexibles para potenciar las
-            habilidades comunicativas de los equipos, combinando inglés general
-            y de negocios.
-          </p>
-        </div>
+      <p className="subtitle-plans underline-zigzag ">
+        Te ofrecemos opciones diseñadas para cada necesidad y objetivo:
+      </p>
+      <div className="underline">
+        <img src="/subrayado.jpg" alt="subrayado" />
+      </div>
+      <section className="container-card-and-plans">
+        <div className="container-card">
+          <a
+            className={plan === "corporate" ? "card-plan-active" : "card-plan"}
+            onClick={() => setPlan("corporate")}
+          >
+            <div className="content-icon-plan-corporate">
+              <FcFactory size={40} />
+            </div>
+            <div>
+              <h2 className="title-plan">Plan Corporativo</h2>
+              <p className="text-plan">
+                Inglés flexible para potenciar equipos en el ámbito laboral y
+                social.
+              </p>
+            </div>
+            <FcNext size={40} />
+          </a>
 
-        <div className="card-plan">
-          <div className="content-icon-plan-intensive">
-            <IoMdSpeedometer className="icon-plan" />
-          </div>
-          <h2>Intensivo</h2>
-          <p className="text-plan">
-            El plan intensivo fortalece gramática, listening, speaking y
-            vocabulario con evaluaciones frecuentes, ideal para lograr objetivos
-            rápidos con dos horas semanales.
-          </p>
-        </div>
+          <a
+            className={plan === "intensive" ? "card-plan-active" : "card-plan"}
+            onClick={() => setPlan("intensive")}
+          >
+            <div className="content-icon-plan-calendar">
+              <FcCalendar size={40} />
+            </div>
+            <div>
+              <h2 className="title-plan">Plan Intensivo</h2>
+              <p className="text-plan">
+                Entrenamiento intensivo de inglés para objetivos puntuales y
+                consolidación rápida.
+              </p>
+            </div>
+            <FcNext size={48} />
+          </a>
 
-        <div className="card-plan">
-          <div className="content-icon-plan-certificate">
-            <FaAward className="icon-plan" />
-          </div>
-
-          <h2>Cert. Internacionales</h2>
-          <p className="text-plan">
-            La empresa decidirá si los participantes rinden exámenes
-            internacionales, gestionados por la coordinación. El programa puede
-            ejecutarse sin rendir, manteniendo el mismo nivel de evaluación.
-          </p>
+          <a
+            className={
+              plan === "certifications" ? "card-plan-active" : "card-plan"
+            }
+            onClick={() => setPlan("certifications")}
+          >
+            <div className="content-icon-plan-globe">
+              <FcGlobe size={40} />
+            </div>
+            <div>
+              <h2 className="title-plan">Certificaciones Internacionales</h2>
+              <p className="text-plan">
+                Opcionalidad de exámenes internacionales con gestión completa de
+                inscripciones.
+              </p>
+            </div>
+            <FcNext size={48} />
+          </a>
+          <a
+            className={
+              plan === "conversation" ? "card-plan-active" : "card-plan"
+            }
+            onClick={() => setPlan("conversation")}
+          >
+            <div className="content-icon-plan-collaboration">
+              <FcCollaboration size={40} />
+            </div>
+            <div>
+              <h2 className="title-plan">Conversacion</h2>
+              <p className="text-plan">
+                Práctica oral activa en un ambiente relajado, ideal para
+                mantener la fluidez en inglés.
+              </p>
+            </div>
+            <FcNext size={48} />
+          </a>
         </div>
-
-        <div className="card-plan">
-          <div className="content-icon-plan-conversation">
-            <FaComments className="icon-plan" />
-          </div>
-          <h2>Conversación</h2>
-          <p className="text-plan">
-            El plan de conversación practica el habla en un ambiente relajado,
-            ideal para mantener la fluidez con debates y dramatizaciones, a
-            veces con coaches nativos.
-          </p>
-        </div>
+        <section className="section-plans">
+          {plan !== "" && (
+            <div className="content-plan">
+              <h2 className="title-plan-section">{plans[plan].title}</h2>
+              <p className="text-plan-section">{plans[plan].text}</p>
+            </div>
+          )}
+        </section>
       </section>
     </div>
   );
