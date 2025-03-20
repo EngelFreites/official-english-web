c7a463f5c3bd92a0779f5feef24d0df5dbdf4790
Engelbert <engelfreites97@gmail.com>
2025-03-20 00:05:02 -0300

    feat(plans): create json with information about plans

---
+++ src/data/plans.js
@@ -0,0 +1,69 @@
+const plans = {
+  corporate: {
+    title: "Plan Corporativo",
+    text: `Nuestro servicio es el indicado para las empresas que quieren potenciar las
+    habilidades de sus equipos, no sólo porque saben que los llevará a obtener
+    mejores resultados e impulsar sus negocios, si no también porque saben que
+    una persona en constante aprendizaje, es una persona calificada y motivada
+    para crecer dentro de la empresa.
+    Ofrecemos sesiones individuales y grupales en las que practicamos un
+    "blend"
+    de inglés general y de negocios, para que los participantes puedan
+    desenvolverse en una reunión de trabajo, pero también logren mantener
+    conversaciones fuera de la oficina con clientes, en viajes de trabajo y otras
+    actividades que no se reduzcan sencillamente a un lenguaje empresarial.
+    Diseñamos un plan de aprendizaje flexible que busca primero satisfacer las
+    necesidades inmediatas de comunicación, para luego ahondar en los detalles
+    que hacen al discurso convincente y persuasivo.`,
+  },
+  intensive: {
+    title: "Plan Intensivo",
+    text: `Se practican habilidades de gramática, listening,
+    speaking y vocabulario. Tal como lo indica el nombre,
+    el ritmo de la clase es intenso, veloz, y se prioriza la
+    consolidación de conocimientos. El coach evaluará al
+    alumno con frecuencia, idealmente al finalizar cada
+    unidad de aprendizaje o al completar dos de ellas.
+    Es elegido por personas que necesitan alcanzar un
+    objetivo puntual y que cuentan con altos niveles de
+    concentración, así como también con el tiempo para
+    comprometerse con dos horas semanales con una
+    asistencia del 90%.`,
+  },
+  certifications: {
+    title: "Certificaciones Internacionales",
+    text: `La empresa podrá decidir si los participantes
+    tendrán derecho a rendir los exámenes
+    internacionales. La coordinación se
+    encargará de las inscripciones, a abonar por
+    la empresa. Para ello, deberán tomar la
+    decisión con antelación, dado que las
+    inscripciones a exámenes internacionales se
+    cierran en momentos específicos del año.
+    Se podrá ejecutar el mismo programa sin
+    necesidad de presentarse a rendir. La
+    consultora evaluará con el mismo criterio y
+    escrutinio el nivel.
+    `,
+  },
+  conversation: {
+    title: "Conversacion",
+    text: `Se practica el lenguaje oral a través del
+    desarrollo de temas de interés general,
+    debates, dramatizaciones, etc. El participante
+    toma un rol activo. Todo esto tiene lugar en un
+    ambiente relajado que facilita el aprendizaje..
+    Es el plan preferido por aquellos que tienen
+    buen dominio del idioma pero no se exponen
+    de manera frecuente al mismo y eligen
+    practicar semanalmente para no perder su
+    fluidez.
+    En ocasiones recomendamos coaches nativos
+    que contribuyan con un entrenamiento más
+    exhaustivo sobre la comprensión de distintos
+    acentos y modismos típicos de sus regiones
+    nativas.`,
+  },
+};
+
+export default plans;
