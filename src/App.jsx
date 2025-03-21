9eeb376abc54d0d1cef9f81d9600955287c0e7ab
Engelbert <engelfreites97@gmail.com>
2025-03-21 15:09:03 -0300

    fear(vercel): configuration vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import Home from "./pages/Home/Home";
 import Home2 from "./pages/Home2/Home2";
-import { Routes, Route } from "react-router";
+import { Routes, Route, HashRouter } from "react-router";
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Home />} key="home" />
-        <Route path="/home2" element={<Home2 />} key="home" />
-      </Routes>
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<Home />} key="home" />
+          <Route path="/home2" element={<Home2 />} key="home" />
+        </Routes>
+      </HashRouter>
     </>
   );
 }
