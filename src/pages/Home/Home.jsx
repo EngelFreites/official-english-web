14fddf7a91abfe11964a184d0b6be785276da2a8
Engelbert <engelfreites97@gmail.com>
2025-03-20 23:00:27 -0300

    feat(home): implement header and plans

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,22 +1,28 @@
 import Button from "../../component/Button/Button";
+import Plans from "../../component/Plans/Plans";
 import "./home.css";
+import Header from "../../component/Header/Header";
 export default function Home() {
   return (
-    <div className="layout">
-      <main className="home">
-        <h1>
-          <span className="highlight">🚀 Domina el inglés</span>y abre un mundo
-          de oportunidades 🌍
-        </h1>
-        <h2>
-          Aprende de manera rápida, divertida y efectiva. ¡Comienza hoy y
-          transforma tu futuro!
-        </h2>
-        <Button>Start Now</Button>
-      </main>
-      <secction className="content-img">
-        <div className="img"></div>
-      </secction>
-    </div>
+    <>
+      <Header />
+      <div className="layout">
+        <main className="home">
+          <h1>
+            <span className="highlight">🚀 Domina el inglés</span>y abre un
+            mundo de oportunidades 🌍
+          </h1>
+          <h2>
+            Aprende de manera rápida, divertida y efectiva. ¡Comienza hoy y
+            transforma tu futuro!
+          </h2>
+          <Button>Start Now</Button>
+        </main>
+        <secction className="content-img">
+          <div className="img"></div>
+        </secction>
+        <Plans />
+      </div>
+    </>
   );
 }
