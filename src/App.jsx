f169ac34a1e41de5ac648bf9d3d29780a2a4364f
Engelbert <engelfreites97@gmail.com>
2025-03-21 15:21:59 -0300

    fix(vercel): control routes in vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
 import Home from "./pages/Home/Home";
 import Home2 from "./pages/Home2/Home2";
-import { Routes, Route, HashRouter } from "react-router";
+import { Routes, Route } from "react-router";
 function App() {
   return (
     <>
-      <HashRouter>
-        <Routes>
-          <Route path="/" element={<Home />} key="home" />
-          <Route path="/home2" element={<Home2 />} key="home" />
-        </Routes>
-      </HashRouter>
+      <Routes>
+        <Route path="/" element={<Home />} key="home" />
+        <Route path="/home2" element={<Home2 />} key="home" />
+      </Routes>
     </>
   );
 }
