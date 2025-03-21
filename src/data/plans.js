71904645ad3d1df6920182d0c36796f02a7ad40f
Engelbert <engelfreites97@gmail.com>
2025-03-21 14:18:25 -0300

    feat(plansList): add planList to json

--- src/data/plans.js
+++ src/data/plans.js
@@ -1,15 +1,7 @@
-const plans = {
+export const plans = Object.entries({
   corporate: {
     title: "Plan Corporativo",
-    text: `Nuestro servicio es el indicado para las empresas que quieren potenciar las
-    habilidades de sus equipos, no sólo porque saben que los llevará a obtener
-    mejores resultados e impulsar sus negocios, si no también porque saben que una persona en constante aprendizaje, es una persona calificada y motivada para crecer dentro de la empresa.\n\nOfrecemos sesiones individuales y grupales en las que practicamos un
-    "blend" de inglés general y de negocios, para que los participantes puedan
-    desenvolverse en una reunión de trabajo, pero también logren mantener
-    conversaciones fuera de la oficina con clientes, en viajes de trabajo y otras
-    actividades que no se reduzcan sencillamente a un lenguaje empresarial.\n
-    Diseñamos un plan de aprendizaje flexible que busca primero satisfacer las
-    necesidades inmediatas de comunicación, para luego ahondar en los detalles que hacen al discurso convincente y persuasivo.`,
+    text: `Nuestro servicio es el indicado para las empresas que quieren potenciar las habilidades de sus equipos, no sólo porque saben que los llevará a obtener mejores resultados e impulsar sus negocios, si no también porque saben que una persona en constante aprendizaje, es una persona calificada y motivada para crecer dentro de la empresa.\n\nOfrecemos sesiones individuales y grupales en las que practicamos un "blend" de inglés general y de negocios, para que los participantes puedan desenvolverse en una reunión de trabajo, pero también logren mantener conversaciones fuera de la oficina con clientes, en viajes de trabajo y otras actividades que no se reduzcan sencillamente a un lenguaje empresarial.\n\nDiseñamos un plan de aprendizaje flexible que busca primero satisfacer las necesidades inmediatas de comunicación, para luego ahondar en los detalles que hacen al discurso convincente y persuasivo.`,
   },
   intensive: {
     title: "Plan Intensivo",
@@ -21,9 +13,33 @@ const plans = {
   },
   conversation: {
     title: "Conversacion",
-    text: `Se practica el lenguaje oral a través del desarrollo de temas de interés general, debates, dramatizaciones, etc. El participantetoma un rol activo. Todo esto tiene lugar en un ambiente relajado que facilita el aprendizaje.\n\n Es el plan preferido por aquellos que tienen buen dominio del idioma pero no se exponen de manera frecuente al mismo y eligen
-    practicar semanalmente para no perder su fluidez.\n\n En ocasiones recomendamos coaches nativos que contribuyan con un entrenamiento más exhaustivo sobre la comprensión de distintos acentos y modismos típicos de sus regiones nativas.`,
+    text: `Se practica el lenguaje oral a través del desarrollo de temas de interés general, debates, dramatizaciones, etc. El participantetoma un rol activo. Todo esto tiene lugar en un ambiente relajado que facilita el aprendizaje.\n\n Es el plan preferido por aquellos que tienen buen dominio del idioma pero no se exponen de manera frecuente al mismo y eligen practicar semanalmente para no perder su fluidez.\n\n En ocasiones recomendamos coaches nativos que contribuyan con un entrenamiento más exhaustivo sobre la comprensión de distintos acentos y modismos típicos de sus regiones nativas.`,
   },
-};
+});
 
-export default plans;
+export const plansList = Object.entries({
+  corporate: {
+    icon: "corporate",
+    title: "Plan Corporativo",
+    text: "Inglés flexible para potenciar equipos en el ámbito laboral y social.",
+    backgroundColor: "bg-blue-200",
+  },
+  intensive: {
+    icon: "intensive",
+    title: "Plan Intensivo",
+    text: "Entrenamiento intensivo de inglés para objetivos puntuales y consolidación rápida.",
+    backgroundColor: "bg-yellow-200",
+  },
+  certifications: {
+    icon: "certifications",
+    title: "Certificaciones Internacionales",
+    text: "Opcionalidad de exámenes internacionales con gestión completa de inscripciones.",
+    backgroundColor: "bg-red-200",
+  },
+  conversation: {
+    icon: "conversation",
+    title: "Conversación",
+    text: "Práctica oral activa en un ambiente relajado, ideal para mantener la fluidez en inglés.",
+    backgroundColor: "bg-green-200",
+  },
+});
