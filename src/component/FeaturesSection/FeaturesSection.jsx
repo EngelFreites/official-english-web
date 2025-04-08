9fed5052b53540373cbf7cc5bf1484760dd2432f
Engelbert <engelfreites97@gmail.com>
2025-04-08 17:56:39 -0300

    feat(FeaturesSection): add scroll animation and title

--- src/component/FeaturesSection/FeaturesSection.jsx
+++ src/component/FeaturesSection/FeaturesSection.jsx
@@ -1,20 +1,40 @@
 import { FaUsers, FaBookOpen, FaChalkboardTeacher, FaGlobeAmericas } from 'react-icons/fa';
-import { motion } from "framer-motion";
+import { motion, useAnimation } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+import { useEffect } from "react";
+
 export default function FeaturesSection() {
+
+  const controls = useAnimation();
+  const [ref, inView] = useInView({ triggerOnce: true });
+
+  useEffect(() => {
+    if (inView) {
+      controls.start({ opacity: 1, y: 0 });
+    }
+  }, [inView, controls]);
+
+
   return (
-    <section className=" relative max-w-7xl flex  justify-center items-center mb-32 mx-auto py-16 px-6 md:px-16">
+    <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>
+      <h1 className="text-4xl font-black text-gray-800 relative inline-block mb-2">
+        Ventajas Clave
+        <span className="absolute left-0 bottom-1 w-64 h-2 bg-yellow-200 opacity-60 rounded-md"></span>
+      </h1>
       
-      
-      <div className="absolute top-20 right-0 w-40 h-40 bg-blue-200 rounded-full z-[-1]"></div>
+      <section className=" relative flex  justify-center items-center s mx-auto py-16 px-6 md:px-16">
+        <div className="absolute top-20 right-0 w-40 h-40 bg-blue-200 rounded-full z-[-1]"></div>
         <div className="flex gap-8">
-      
-          <motion.div
-            className=" animate-scroll flex  gap-2 h-12 px-4 rounded-4xl bg-indigo-50  items-center  shadow-md transition-all duration-300 ease-in-out hover:shadow-xl hover:scale-105"
-            initial={{ opacity: 0, y: 20 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.5, delay: 0.1 }}
-            whileHover={{ scale: 1.05 }}
-          >
+        <motion.div
+          ref={ref}
+          initial={{ y: -80, opacity: 0 }}
+          animate={controls}
+          transition={{
+            y: { duration: 0.5, ease: "easeOut" },
+            opacity: { duration: 0.3, delay: 0.5 }, 
+          }}
+          className="flex items-center bg-indigo-50 gap-2 h-12 px-4 rounded-4xl shadow-md transition-all duration-300 ease-in-out hover:shadow-xl hover:scale-105"
+        >
             <div className="bg-indigo-50  rounded-full ">
               <FaChalkboardTeacher className="text-indigo-600 text-xl" />
             </div>
@@ -23,11 +43,14 @@ export default function FeaturesSection() {
           </motion.div>
 
           <motion.div
-            className=" animate-scroll flex  bg-pink-50 items-center gap-2 h-12 px-4 rounded-4xl shadow-md transition-all duration-300 ease-in-out hover:shadow-xl hover:scale-105"
-            initial={{ opacity: 0, y: 20 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.5, delay: 0.2 }}
-            whileHover={{ scale: 1.05 }}
+            ref={ref}
+            initial={{ y: -60, opacity: 0 }}
+            animate={controls}
+            transition={{
+              y: { duration: 0.4, ease: "easeOut" },
+              opacity: { duration: 0.3, delay: 0.5 }, 
+            }}
+            className="flex items-center bg-pink-50 gap-2 h-12 px-4 rounded-4xl shadow-md transition-all duration-300 ease-in-out hover:shadow-xl hover:scale-105"
           >
             <div className="bg-pink-50  rounded-full">
               <FaBookOpen className="text-pink-600 text-xl" />
@@ -35,13 +58,15 @@ export default function FeaturesSection() {
           
             <p className="text-pink-600  text-center text-sm ">Material Actualizado</p>
           </motion.div>
-
           <motion.div
-            className=" animate-scroll flex items-center bg-green-50  gap-2 h-12 px-4 rounded-4xl shadow-md transition-all duration-300 ease-in-out hover:shadow-xl hover:scale-105"
-            initial={{ opacity: 0, y: 20 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.5, delay: 0.3 }}
-            whileHover={{ scale: 1.05 }}
+            ref={ref}
+            initial={{ y: -40, opacity: 0 }}
+            animate={controls}
+            transition={{
+              y: { duration: 0.3, ease: "easeOut" },
+              opacity: { duration: 0.3, delay: 0.5 }, 
+            }}
+            className="flex items-center bg-green-50 gap-2 h-12 px-4 rounded-4xl shadow-md transition-all duration-300 ease-in-out hover:shadow-xl hover:scale-105"
           >
             <div className="bg-green-50  rounded-full ">
               <FaUsers className="text-green-600 text-xl" />
@@ -49,13 +74,15 @@ export default function FeaturesSection() {
           
             <p className="text-green-600 text-sm">Coaches Expertos</p>
           </motion.div>
-
           <motion.div
-            className=" animate-scroll flex bg-yellow-50 items-center  gap-2 h-12 px-4 rounded-4xl shadow-md transition-all duration-300 ease-in-out hover:shadow-xl hover:scale-105"
-            initial={{ opacity: 0, y: 20 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.5, delay: 0.4 }}
-            whileHover={{ scale: 1.05 }}
+            ref={ref}
+            initial={{ y: -35, opacity: 0 }}
+            animate={controls}
+            transition={{
+              y: { duration: 0.2, ease: "easeOut" },
+              opacity: { duration: 0.3, delay: 0.5 }, // aparece cuando ya casi llega
+            }}
+            className="flex items-center bg-yellow-50 gap-2 h-12 px-4 rounded-4xl shadow-md transition-all duration-300 ease-in-out hover:shadow-xl hover:scale-105"
           >
             <div className="bg-yellow-50 rounded-full ">
               <FaGlobeAmericas className="text-yellow-600 text-xl" />
@@ -64,6 +91,8 @@ export default function FeaturesSection() {
             <p className="text-center text-yellow-600 text-sm">Inmersión Cultural</p>
           </motion.div>
         </div>
-      </section>  
+      </section> 
+    </div>
+     
   )
 }
