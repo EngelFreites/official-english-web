a594e90fd9710009b34789f0e0301b94c6388dee
Engelbert <engelfreites97@gmail.com>
2025-04-22 15:09:40 -0300

    rename(component): change Home2 to Home for clarity

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import Home2 from "./pages/Home2/Home2";
+import Home from "./pages/Home2/Home2";
 import { Routes, Route } from "react-router";
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home2 />} key="home" />
+        <Route path="/" element={<Home />} key="home" />
       </Routes>
     </>
   );
