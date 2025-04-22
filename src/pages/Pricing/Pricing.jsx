dacdd50ab291954a57da958398f7769f5b75770b
Engelbert <engelfreites97@gmail.com>
2025-04-22 15:00:23 -0300

    feat(pricing): add footer

--- src/pages/Pricing/Pricing.jsx
+++ src/pages/Pricing/Pricing.jsx
@@ -1,9 +1,10 @@
-import PricingPlans from "../../component/PricingPlans/PricingPlans"
+import PricingPlans from "../../component/PricingPlans/PricingPlans";
+import Footer from "../../component/Footer/Footer";
 export default function Pricing() {
   return (
     <>
       <PricingPlans />
+      <Footer />
     </>
-
-  )
+  );
 }
