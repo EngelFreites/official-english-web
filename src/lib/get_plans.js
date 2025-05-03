399a75cd097b29a40c2d99570f6aff69046011f2
Engelbert <engelfreites97@gmail.com>
2025-05-02 22:25:19 -0300

    chore(strapi): remove unused Strapi config files

--- src/lib/get_plans.js
+++
@@ -1,7 +0,0 @@
-import fetchStrapi from "./strapi.js";
-export default function get_plans() {
-  return fetchStrapi("/plans").then((res) => {
-    console.log("Strapi response:", res);
-    return res;
-  });
-}
