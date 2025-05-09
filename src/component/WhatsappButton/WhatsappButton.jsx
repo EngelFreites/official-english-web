6397267f346d7973466995afbe34beafa5f5ffaa
Engelbert <engelfreites97@gmail.com>
2025-05-08 23:19:59 -0300

    feat: create whatsapp button

---
+++ src/component/WhatsappButton/WhatsappButton.jsx
@@ -0,0 +1,17 @@
+import { FaWhatsapp } from "react-icons/fa";
+
+export default function WhatsappButton() {
+  return (
+    <a
+      href="https://wa.me/+5491158120567"
+      target="_blank"
+      rel="noopener noreferrer"
+      className="fixed bottom-16 right-16 z-999"
+    >
+      <FaWhatsapp
+        className="h-18 w-18 text-white stroke-black stroke-[] "
+        color="green"
+      />
+    </a>
+  );
+}
