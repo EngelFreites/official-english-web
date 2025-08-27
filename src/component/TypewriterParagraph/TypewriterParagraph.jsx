40986ff19e95027d97b08bdc0be9af883e699de9
Engelbert <engelfreites97@gmail.com>
2025-08-26 22:05:54 -0300

    refactor: change TypewriterParagraph to build displayed text incrementally

--- src/component/TypewriterParagraph/TypewriterParagraph.jsx
+++ src/component/TypewriterParagraph/TypewriterParagraph.jsx
@@ -1,28 +1,23 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
-export default function TypewriterParagraph({children}) {
-  const [displayedText, setDisplayedText] = useState('');
+export default function TypewriterParagraph({ children }) {
+  const [index, setIndex] = useState(0);
 
   useEffect(() => {
-    let index = 0;
-    const interval = setInterval(() => {
-      setDisplayedText((prev) => prev + children.charAt(index));
-      index++;
-      if (index >= children.length) clearInterval(interval);
-    }, 10);
-    return () => clearInterval(interval);
-  }, []);
+    if (index < children.length) {
+      const interval = setInterval(() => {
+        setIndex((prev) => prev + 1);
+      }, 5);
+      return () => clearInterval(interval);
+    }
+  }, [index, children.length]);
 
   return (
-    <div className='h-[125px]'>
-      <p
-      className="text-xl text-gray-600 mb-8 whitespace-pre-line"
-
-      >
-        {displayedText}
+    <div className="h-[125px]">
+      <p className="text-xl text-gray-600 mb-8 whitespace-pre-line">
+        {children.slice(0, index)}
         <span className="inline-block animate-pulse">|</span>
       </p>
     </div>
-    
   );
 }
