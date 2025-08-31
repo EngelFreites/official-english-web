b928c25c9c5f2dffb1ef8047e24b7d719e94a1d9
Engelbert <engelfreites97@gmail.com>
2025-08-31 18:33:55 -0300

    style: delete high

--- src/component/TypewriterParagraph/TypewriterParagraph.jsx
+++ src/component/TypewriterParagraph/TypewriterParagraph.jsx
@@ -13,8 +13,8 @@ export default function TypewriterParagraph({ children }) {
   }, [index, children.length]);
 
   return (
-    <div className="h-[125px]">
-      <p className="text-xl text-gray-600 mb-8 whitespace-pre-line">
+    <div>
+      <p className="text-xl text-gray-600 whitespace-pre-line">
         {children.slice(0, index)}
         <span className="inline-block animate-pulse">|</span>
       </p>
