9d8da1142d07d46d6555731d93b3d3559f6bf446
Engelbert <engelfreites97@gmail.com>
2025-09-22 01:48:32 -0300

    style: add padding in x to sections

--- src/component/WeAre/WeAre.jsx
+++ src/component/WeAre/WeAre.jsx
@@ -4,7 +4,7 @@ export default function WeAre() {
   return (
     <section
       id="aboutUs"
-      className="flex flex-col py-20 sm:px-6 lg:px-8  max-w-7xl mx-auto "
+      className="flex flex-col py-20 px-4 sm:px-6 lg:px-8  max-w-7xl mx-auto "
     >
       <Title>¿Quiénes somos?</Title>
 
